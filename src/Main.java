6edef46aab1fda195392196a6a1ff048ee2878b3
THEO2500 <worldpeace871219@gmail.com>
2017-10-16 21:10:48 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        float a=3.2f;
+        a=in.nextFloat();
+        System.out.printf("%f\n",a);
+        System.out.printf("%2.7f\n",a);
+        // %2.7f\n的2是代表甚麼?
+
+
+
+
+
+    }
+
+}
